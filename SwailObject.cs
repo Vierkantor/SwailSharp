ea1215f20ac9fdbb3860d49f96f21f7f766c6c54
Vierkantor <vierkantor@vierkantor.com>
2013-11-28 20:55:11 +0100

    Filled in the SwailObject class
    Now you can make an object that stores data! OOP, here we come!

--- SwailObject.cs
+++ SwailObject.cs
@@ -1,11 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 namespace SwailSharp
 {
+	// a thing that code in Swail can possibly reach
+	// Most things should be SwailObjects, even things like the Parser or ShellWindow
 	public class SwailObject
 	{
-		public SwailObject ()
+		// Swail is object-oriented now: every object has its own members
+		public IDictionary<string, SwailObject> Members;
+		// the name under which the object was defined
+		public string Name;
+
+		public SwailObject(string name)
 		{
+			this.Name = name;
+			this.Members = new Dictionary<string, SwailObject>();
 		}
 	}
 }
