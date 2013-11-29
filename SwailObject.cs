ebe6ed59d133a4d5dca3123fa9fbd3eea7656db0
Vierkantor <vierkantor@vierkantor.com>
2013-11-29 16:12:30 +0100

    More functionality for SwailObject: "this" member and "None" object

--- SwailObject.cs
+++ SwailObject.cs
@@ -5,8 +5,9 @@ namespace SwailSharp
 {
 	// a thing that code in Swail can possibly reach
 	// Most things should be SwailObjects, even things like the Parser or ShellWindow
-	public class SwailObject
-	{
+	public class SwailObject {
+		public static SwailObject None = new SwailObject("None");
+
 		// Swail is object-oriented now: every object has its own members
 		public IDictionary<string, SwailObject> Members;
 		// the name under which the object was defined
@@ -16,7 +17,8 @@ namespace SwailSharp
 		{
 			this.Name = name;
 			this.Members = new Dictionary<string, SwailObject>();
+			
+			this.Members.Add("this", this);
 		}
 	}
 }
-
