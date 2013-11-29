0e335bcfe9faf596830fa95541a8cd7c8e99a2f6
Vierkantor <vierkantor@vierkantor.com>
2013-11-29 18:03:18 +0100

    Added SwailString class
    Even less utility and even more wrappyness

---
+++ SwailString.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SwailSharp {
+	public class SwailString : SwailObject {
+		public string Value;
+		
+		public SwailString(string value) {
+			this.Value = value;
+		}
+	}
+}
+
